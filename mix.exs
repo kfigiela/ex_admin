1059eb5ce3eb75dfdaf1b52efc2c5ac0bb365093
Miron Markowski <majron15@gmail.com>
2017-08-11 13:06:45 +0200

    Match phoenix_html version with xain

--- mix.exs
+++ mix.exs
@@ -40,7 +40,7 @@ defmodule ExAdmin.Mixfile do
     [
       {:decimal, "~> 1.0"},
       {:phoenix, "~> 1.2"},
-      {:phoenix_html, "~> 2.6"},
+      {:phoenix_html, "~> 2.10"},
       {:ecto, "~> 2.1"},
       {:phoenix_ecto, "~> 3.2"},
       {:postgrex, "~> 0.13", only: :test},
