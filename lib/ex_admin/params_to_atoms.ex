d14f8cc30d8807881e0dd94ff227e45461ee9fae
Kamil Figiela <kamil.figiela@gmail.com>
2017-08-16 15:13:45 +0200

    Remove strange casting _id params to numbers.

--- lib/ex_admin/params_to_atoms.ex
+++ lib/ex_admin/params_to_atoms.ex
@@ -3,8 +3,7 @@ defmodule ExAdmin.ParamsToAtoms do
   require Logger
 
   def filter_params(params, schema) do
-    params_string_fields_to_integer(params)
-    |> params_to_atoms(schema)
+    params_to_atoms(params, schema)
   end
 
   def params_to_atoms(params, schema) do
@@ -30,32 +29,4 @@ defmodule ExAdmin.ParamsToAtoms do
 
   defp _to_atom(key) when is_atom(key), do: key
   defp _to_atom(key), do: String.to_atom(key)
-
-  defp params_string_fields_to_integer(params) do
-    list = for {key,value} <- params, do: _replace_integers(key, value)
-    Enum.into list, Map.new
-  end
-
-  @integer_keys ~r/_id$|^id$|page|page_size/
-
-  defp _replace_integers(key, " " <> rest), do: _replace_integers(key, rest)
-  defp _replace_integers(key, ""), do: {key, nil}
-  defp _replace_integers(key, %Plug.Upload{} = value), do: {key, value}
-  defp _replace_integers(key, value) when is_integer(value), do: {key, value}
-  defp _replace_integers(key, value) when is_map(value), do: {key, params_string_fields_to_integer(value)}
-  defp _replace_integers(key, value) when is_atom(key) do
-    {_, value} = _replace_integers(Atom.to_string(key), value)
-    {key, value}
-  end
-  defp _replace_integers(key, value) when is_binary(key) do
-    if Regex.match?(@integer_keys, key) do
-      case {Regex.match?(~r/^[0-9]+$/, value), value} do
-        {false, ""} -> {key, nil}
-        {true, _} -> {key, String.to_integer(value)}
-        _ -> {key, value}
-      end
-    else
-      {key, value}
-    end
-  end
 end
